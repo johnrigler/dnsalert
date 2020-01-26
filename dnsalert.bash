844e48e611e5278bfc05556dc39dde295bcb7e39
John Rigler <john@rigler.org>
2020-01-26 18:34:40 +0000

    Added alert for gofundspark.com

--- dnsalert.bash
+++ dnsalert.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+if [[ $CIRCLECI == 'true' ]]
+then
+DIR=/home/circleci/project
+else
+DIR=/root/dnsalert
+fi
+
+for X in 1 2 3 4 5 6 7 8 9 10
+do
+dig gofundspark.com MX +short
+sleep 5
+done | sort -u 
+
 return 0
