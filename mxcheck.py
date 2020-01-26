9106e2b7ad973c4e0eed55845bb2044898b5af38
John Rigler <john@rigler.org>
2020-01-26 22:55:26 +0000

    .

---
+++ mxcheck.py
@@ -0,0 +1,12 @@
+print("xx")
+fqdn = sys.argv[1]
+
+list = ([split])
+
+    cnt = 0
+    for n, c in enumerate(list):
+       x = b58_dcmap.find(c)
+       if x!= -1:
+          list[cnt] = b58_digits[x]
+       if c == 'i':
+
