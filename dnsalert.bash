d8b74abfd4848307000cceebf65c5fe40d897b0d
John Rigler <john@rigler.org>
2020-01-26 19:30:54 +0000

    Add test for multiple MX records

--- dnsalert.bash
+++ dnsalert.bash
@@ -16,7 +16,26 @@ curl http://dime.cash/dig_mx.php?host="$1" 2>/dev/null
 for X in 1 2 3 4 5 
 do
 dig_mx gofundspark.com
-sleep 2
+sleep 0
 done | sort -u > $$
 
-cat $$
+cat $$ | grep "0 gofundspark-com.mail.protection.outlook.com."
+
+
+# Test for a good entry
+
+if [[ $? -ne 0 ]]
+  then
+  exit 1
+  fi
+
+# Test for old records
+
+cat $$ | grep -v "0 gofundspark-com.mail.protection.outlook.com."
+
+if [[ $? -eq 0 ]]
+  then
+  exit 1
+  fi
+ 
+rm $$
