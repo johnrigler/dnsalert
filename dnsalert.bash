4d5fa697ec314d3cf2111b4c12aa30b484e5a5a5
John Rigler <john@rigler.org>
2020-01-26 18:46:16 +0000

    .

--- dnsalert.bash
+++ dnsalert.bash
@@ -3,14 +3,17 @@
 if [[ $CIRCLECI == 'true' ]]
 then
 DIR=/home/circleci/project
+apt-get install dnsutils
 else
 DIR=/root/dnsalert
 fi
 
-for X in 1 2 3 4 5 6 7 8 9 10
+COUNT=$(for X in 1 2 3 4 5 6 7 8 9 10
 do
 dig gofundspark.com MX +short
-sleep 5
-done | sort -u 
+sleep 0
+done | sort -u )
 
-return 0
+let COUNT=COUNT-1
+
+exit $COUNT 
