92809f20f298a7114629b0b29d238204021f67fb
john.rigler <john@rigler.org>
2020-01-26 20:52:36 -0600

    added mxcheck.txt with a list of hostnames

--- mxcheck.py
+++ mxcheck.py
@@ -1,17 +1,19 @@
 
-# fqdn = sys.argv[1]
-
-name= "gofundspark.com"
+lineList = [line.rstrip('\n') for line in open("mxcheck.txt")]
 
 split = name.split(".")
 count = len(split) - 2
 newname = ""
 
-for i in split:
-    newname = newname + i
-    if count == 0:
-      newname = newname + "-"
-    elif count > 0:
-      newname = newname + "."
-    count = count - 1
-print(newname)
+for host in lineList:
+    split = host.split(".")
+    count = len(split) -2
+    newname = ""
+    for i in split:
+        newname = newname + i
+        if count == 0:
+          newname = newname + "-"
+        elif count > 0:
+          newname = newname + "."
+        count = count - 1
+    print(newname)
